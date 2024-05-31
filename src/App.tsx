9619f62f9b4e86036c8027145ae0eb347d1788b1
何奇 <liuzhenying@MacBook-Pro.local>
2024-05-31 11:37:41 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+
+    return (
+        <div className='home'>
+            <div className='content'>
+                <div className='title'>智慧蓉城工业与新经济城运分中心演示汇报“打擂台”</div>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
